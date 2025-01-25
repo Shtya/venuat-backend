195afbd55b37eaa5a845f6fa1606d48632d69d8c
ahmed <shtya54@gmail.com>
2025-01-25 11:34:01 +0200

    vercel.config

--- api/index.js
+++ api/index.js
@@ -1,14 +1,35 @@
-const { NestFactory } = require('@nestjs/core');
-const { AppModule } = require('../dist/app.module');
+// api/index.ts
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from '../src/app.module';
+import { ExpressAdapter } from '@nestjs/platform-express';
+import * as express from 'express';
+import serverlessExpress from '@vendia/serverless-express';
 
-async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.init();
+let cachedServer;
 
-  const server = express();
-  server.use(app.getHttpAdapter().getInstance());
+async function bootstrapServer() {
+  if (!cachedServer) {
+    const expressApp = express();
+    const nestApp = await NestFactory.create(
+      AppModule,
+      new ExpressAdapter(expressApp),
+    );
 
-  return server;
+    // Add your global configurations here (e.g., CORS, pipes, middleware)
+    nestApp.enableCors();
+    nestApp.setGlobalPrefix('api/v1');
+
+    await nestApp.init();
+    cachedServer = serverlessExpress({ app: expressApp });
+  }
+  return cachedServer;
 }
 
-module.exports = bootstrap();
+export const handler = async (
+  event,
+  context,
+  callback,
+) => {
+  const server = await bootstrapServer();
+  return server(event, context, callback);
+};
